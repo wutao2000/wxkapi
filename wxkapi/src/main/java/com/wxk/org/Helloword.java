48738a39b72f147e6daa1fcf2e62d85ff7a06ec3
wutao2000 <erik20130312@163.com>
2017-10-09 09:05:19 +0800

    @test

--- wxkapi/src/main/java/com/wxk/org/Helloword.java
+++ wxkapi/src/main/java/com/wxk/org/Helloword.java
@@ -1,6 +1,18 @@
 package com.wxk.org;
 
+import org.testng.annotations.Test;
+
 
 public class Helloword {
-
+   @Test
+   public void Helloword(){
+	   System.out.println("ok!!!!!!!!!!!!!!!");
+   }
+   @Test
+   public void ABC(){
+	   System.out.println("11111111111111111");
+   }
+	
 }
+
+
